5957cb1b0159fb4bad7e301cf99ac44592d3d702
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-21 09:01:12 -0300

    s6a112: table component #2

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -9,9 +9,9 @@ export default function Tabela(props: TabelaProps){
     function renderHeader(){
         return(
             <tr>
-                <th>Codigo</th>
-                <th>Nome</th>
-                <th>Idade</th>
+                <th className={`text-left p-4`}>Codigo</th>
+                <th className={`text-left p-4`}>Nome</th>
+                <th className={`text-left p-4`}>Idade</th>
             </tr>
         )
     }
@@ -19,14 +19,14 @@ export default function Tabela(props: TabelaProps){
     function renderData(){
         return props.clientes?.map((cliente, i) => {
             return (
-                <tr key={cliente.id}>
-                    <td>
+                <tr key={cliente.id} className={`${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}>
+                    <td className={`text-left p-4`}>
                         {cliente.id}
                     </td>
-                    <td>
+                    <td className={`text-left p-4`}>
                         {cliente.nome}
                     </td>
-                    <td>
+                    <td className={`text-left p-4`}>
                         {cliente.idade}
                     </td>
                 </tr>
@@ -34,8 +34,15 @@ export default function Tabela(props: TabelaProps){
         }) 
     }
     return (
-        <table>
-            <thead>
+        <table className={`
+            rounded-xl
+            overflow-hidden
+            w-full
+        `}>
+            <thead className={`
+              text-gray-100
+                bg-gradient-to-r from-purple-500 to-purple-800
+            `}> 
                 {
                     renderHeader()
                 }
