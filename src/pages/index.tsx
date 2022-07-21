0a139055847b9df91c7a4d7c7f4fff784567201b
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-21 08:24:48 -0300

    s6a108: init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-tr from-purple-500 to-black
+    `}>
+      <span className="text-4xl">Texto</span>
+    </div>
+  )
+}
