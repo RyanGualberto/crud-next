f939c1da826a4016936b5485b59944252e6263a7
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-21 09:25:03 -0300

    s6a113: table component #03

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -1,18 +1,45 @@
 import Cliente from '../core/Cliente'
+import { IconeEdicao, IconeLixo } from './Icones'
 
 interface TabelaProps{
     clientes: Cliente[]
+    clienteSelecionado?: (cliente: Cliente) => void
+    clienteExcluido?: (cliente: Cliente) => void
+
 }
 
 export default function Tabela(props: TabelaProps){
 
+    const showActions = props.clienteExcluido || props.clienteSelecionado
     function renderHeader(){
         return(
             <tr>
                 <th className={`text-left p-4`}>Codigo</th>
                 <th className={`text-left p-4`}>Nome</th>
                 <th className={`text-left p-4`}>Idade</th>
+                {showActions ? <th className={`p-4`}>Acoes</th> : false}  
             </tr>
+        )
+    }
+    
+    function renderActions(cliente: Cliente){
+        return(
+            <td className={`flex justify-center`}>
+                {props.clienteSelecionado ? (
+                    <button 
+                    onClick={() => props.clienteSelecionado?.(cliente) } 
+                    className={`flex justify-center items-center rounded-full p-2 m-1 text-green-600 hover:bg-purple-50`}>
+                        {IconeEdicao}
+                    </button>
+                ) : false}
+                {props.clienteExcluido ? (
+                    <button 
+                    onClick={() => props.clienteExcluido?.(cliente) } 
+                    className={`flex justify-center items-center rounded-full p-2 m-1 text-red-500 hover:bg-purple-50`}>
+                        {IconeLixo}
+                    </button>   
+                ) : false}
+            </td>
         )
     }
 
@@ -29,6 +56,7 @@ export default function Tabela(props: TabelaProps){
                     <td className={`text-left p-4`}>
                         {cliente.idade}
                     </td>
+                    {showActions ?  renderActions(cliente) : false}
                 </tr>
             )
         }) 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,19 @@ import Cliente from "../core/Cliente"
 export default function Home() {
   const clientes = [
     new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 12, '1'),
-    new Cliente('Marcos', 23, '1'),
-    new Cliente('Ronaldo', 87, '1'),
+    new Cliente('Bia', 12, '2'),
+    new Cliente('Marcos', 23, '3'),
+    new Cliente('Ronaldo', 87, '4'),
   ]
+
+  function clienteSelecionado(cliente: Cliente){
+    console.log(cliente.nome);
+    
+  }
+  function clienteExcluido(cliente: Cliente){
+    console.log(cliente.nome);
+    
+  }
 
   return (
     <div className={`
@@ -16,7 +25,11 @@ export default function Home() {
       bg-gradient-to-tr from-purple-500 to-black text-white
     `}>
       <Layout titulo="Cadastro Simples">
-          <Tabela clientes={clientes}></Tabela>
+          <Tabela 
+          clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+          />
       </Layout>
     </div>
   )
