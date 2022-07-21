861129040618d66d00c6307ac61cf079ec5fb77d
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-21 11:09:14 -0300

    s6a120: sign up to firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,37 +3,46 @@ import Tabela from "../components/Tabela"
 import Cliente from "../core/Cliente"
 import Botao from "../components/Botao"
 import Formulario from "../components/Formulario"
-import { useState} from 'react'
+import { useState, useEffect} from 'react'
+import ClienteRepositorio from "../core/ClienteRepositorio"
+import ColecaoCliente from "../back/db/ColecaoCliente"
 
 export default function Home() {
+
+  const repo: ClienteRepositorio = new ColecaoCliente()
   const [visivel, setVisivel] = useState<'tabela' | 'formulario'>('tabela')
+  const [clientes, setClientes] = useState<Cliente[]>([])
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
 
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 12, '2'),
-    new Cliente('Marcos', 23, '3'),
-    new Cliente('Ronaldo', 87, '4'),
-  ]
+
+  useEffect(obterTodos, [])
+  
+  function obterTodos(){
+    repo.obterTodos().then(clientes => {
+        setClientes(clientes)
+        setVisivel('tabela')
+    })
+  }
 
   function clienteSelecionado(cliente: Cliente){
     setCliente(cliente)
     setVisivel('formulario')
   }
-  function clienteExcluido(cliente: Cliente){
-    console.log(cliente.nome);
+  async function clienteExcluido(cliente: Cliente){
+    await repo.excluir(cliente)
+    obterTodos()
   }
 
-  function salvarCliente(cliente: Cliente){
-    console.log(cliente);
-    setVisivel('tabela')  
-  }
-
+  
   function novoCliente(cliente: Cliente){
     setCliente(Cliente.vazio())
     setVisivel('formulario')  
   }
-
+  
+  async function salvarCliente(cliente: Cliente){
+    await repo.salvar(cliente)
+    obterTodos()
+  }
 
   return (
     <div className={`
@@ -46,8 +55,8 @@ export default function Home() {
               <div className={`flex justify-end`}>
                   <Botao 
                   cor="green" 
-                  className="mb-4"
                   onClick={novoCliente}
+                  className="mb-4"
                   > Novo Cliente
                   </Botao>
               </div>
